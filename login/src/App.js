9d2fb3809052b0c92b1f0990a1a3d863c2dac94e
Pheba08 <sspheba08@gmail.com>
2025-01-10 16:04:45 +0530

    login-page-started

---
+++ login/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Adminthree from './components/Adminthree';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Adminthree/>
+      <admin3/>
+      
+    </div>
+  );
+}
+
+export default App;
